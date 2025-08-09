0259e5c01c7927e74a8c2dd6f570c5a741eb8ded
AmirManshadi <amirmanshadi.code@gmail.com>
2025-08-09 17:33:55 +0330

    Refactor components and implement form handling with local storage support

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,93 +1,49 @@
-// Form.tsx
-import { useState, useEffect } from "react";
-import Card from "./card";
+import { EmailInput, TextInput } from "./input";
+import { generateRandomId } from "../utils";
+import type { CardData } from "../types";
+import { useInput } from "../hooks";
 
-type CardData = {
-  id: string;
-  name: string;
-  email: string;
-};
-
-function Form() {
-  const [inputName, setInputName] = useState("");
-  const [inputEmail, setInputEmail] = useState("");
-
-  const [cards, setCards] = useState<CardData[]>(() => {
-    const savedCards = localStorage.getItem("cards");
-    return savedCards ? JSON.parse(savedCards) : [];
-  });
-
-  useEffect(() => {
-    localStorage.setItem("cards", JSON.stringify(cards));
-  }, [cards]);
-
-  const NameUpdater = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputName(e.target.value);
-  };
-
-  const EmailUpdater = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputEmail(e.target.value);
-  };
-
-  const submitHandler = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    if (inputName && inputEmail) {
-      const newCard = {
-        id: Date.now().toString(),
-        name: inputName,
-        email: inputEmail,
-      };
-      setCards([...cards, newCard]);
-      setInputName("");
-      setInputEmail("");
-    }
-  };
-
-  // تابع جدید برای حذف یک کارت با استفاده از id
-  const removeCard = (idToRemove: string) => {
-    const newCards = cards.filter((card) => card.id !== idToRemove);
-    setCards(newCards);
-  };
-
-  return (
-    <>
-      <form
-        onSubmit={submitHandler}
-        className="bg-slate-800 flex flex-col mx-auto my-5 justify-center p-4 rounded-md md:w-2xl"
-      >
-        <input
-          type="text"
-          placeholder="نام"
-          className="my-3 bg-slate-900 p-3 rounded-md text-white"
-          onChange={NameUpdater}
-          value={inputName}
-        />
-        <input
-          type="email"
-          placeholder="ایمیل"
-          className="my-3 bg-slate-900 p-3 rounded-md text-white"
-          onChange={EmailUpdater}
-          value={inputEmail}
-        />
-        <button
-          type="submit"
-          className="bg-slate-600 text-gray-200 p-3 text-xl rounded-md my-3"
-        >
-          تایید
-        </button>
-      </form>
-
-      {cards.map((card) => (
-        <Card
-          key={card.id}
-          name={card.name}
-          email={card.email}
-          onRemove={() => removeCard(card.id)}
-        />
-      ))}
-    </>
-  );
+interface FormProps {
+	onCardAdd: (card: CardData) => void;
 }
 
-export default Form;
+function Form({ onCardAdd }: FormProps) {
+	const [name, setName, onNameChange] = useInput();
+	const [email, setEmail, onEmailChange] = useInput();
+
+	const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		if (name && email) {
+			const newCard = {
+				id: generateRandomId(),
+				name,
+				email
+			};
+
+			onCardAdd(newCard);
+
+			setName("");
+			setEmail("");
+		}
+	};
+
+	return (
+		<form
+			onSubmit={handleFormSubmit}
+			className="bg-slate-800 flex flex-col mx-auto my-5 justify-center p-4 rounded-md md:w-2xl"
+		>
+			<TextInput placeholder="نام" value={name} onChange={onNameChange} />
+			<EmailInput placeholder="ایمیل" value={email} onChange={onEmailChange} />
+
+			<button
+				type="submit"
+				className="bg-slate-600 text-gray-200 p-3 text-xl rounded-md my-3"
+			>
+				تایید
+			</button>
+		</form>
+	);
+}
+
+export { Form };
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,33 @@
-import Form from "../components/form";
+import { useState, useEffect } from "react";
+import type { CardData } from "../types";
+import { List, Form } from "../components";
+
+const LOCAL_STORAGE_KEY = "cards";
 
 function Home() {
-  return (
-    <div className="  p2">
-      <Form />
-    </div>
-  );
+	const [cards, setCards] = useState<CardData[]>(() => {
+		const savedCards = localStorage.getItem(LOCAL_STORAGE_KEY);
+		return savedCards ? JSON.parse(savedCards) : [];
+	});
+
+	useEffect(() => {
+		localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(cards));
+	}, [cards]);
+
+	const handleCardAdd = (card: CardData) => {
+		setCards(prevCards => [...prevCards, card]);
+	};
+
+	const handleCardRemove = (id: string) => {
+		setCards(prevCards => prevCards.filter(card => card.id !== id));
+	};
+
+	return (
+		<div>
+			<Form onCardAdd={handleCardAdd} />
+			<List cards={cards} onCardRemove={handleCardRemove} />
+		</div>
+	);
 }
 
-export default Home;
+export { Home };
